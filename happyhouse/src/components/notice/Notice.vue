816c60fcededbeac29d0f3739f0380ed26a88535
adlgu <adlguswjda@naver.com>
2021-05-25 12:13:17 +0900

    add parent vues

---
+++ happyhouse/src/components/notice/Notice.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <h3 class="text-center">공지사항</h3>
+    </div>
+    <router-view />
+  </div>
+</template>
---
+++ happyhouse/src/components/qna/Qna.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <h3 class="text-center">Q&A</h3>
+    </div>
+    <router-view />
+  </div>
+</template>
