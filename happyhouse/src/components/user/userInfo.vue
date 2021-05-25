c4f170dad4d80482b105c679bafcf6bb4177feb4
adlgu <adlguswjda@naver.com>
2021-05-25 10:59:01 +0900

    update userinfo

--- happyhouse/src/components/user/userInfo.vue
+++ happyhouse/src/components/user/userInfo.vue
@@ -30,7 +30,14 @@
                   >비밀번호
                 </label>
                 <div class="col-md-8">
+                  <label
+                    v-if="!modify"
+                    for="inputName"
+                    class="col-md-3 control-label text-md-right col-form-label"
+                    >{{ user.userPwd }}
+                  </label>
                   <input
+                    v-else
                     type="password"
                     class="form-control"
                     id="userPwd"
@@ -39,7 +46,7 @@
                   />
                 </div>
               </div>
-              <div class="form-group has-feedback row">
+              <div v-if="modify" class="form-group has-feedback row">
                 <label for="pwdcheck" class="col-md-3 control-label text-md-right col-form-label"
                   >비밀번호 확인
                 </label>
@@ -50,7 +57,11 @@
                     id="pwdcheck"
                     ref="pwdcheck"
                     v-model="pwdcheck"
+                    @keyup="checkPwd()"
                   />
+                </div>
+                <div v-if="msgPwd" id="waringMsg" class="mt-3" :class="statusPwdMsg">
+                  {{ msgPwd }}
                 </div>
               </div>
               <div class="form-group has-feedback row">
@@ -60,7 +71,14 @@
                   >이름
                 </label>
                 <div class="col-md-8">
+                  <label
+                    v-if="!modify"
+                    for="inputName"
+                    class="col-md-3 control-label text-md-right col-form-label"
+                    >{{ user.userName }}
+                  </label>
                   <input
+                    v-else
                     type="text"
                     class="form-control"
                     id="userName"
@@ -74,7 +92,14 @@
                   >주소
                 </label>
                 <div class="col-md-8">
+                  <label
+                    v-if="!modify"
+                    for="inputName"
+                    class="col-md-3 control-label text-md-right col-form-label"
+                    >{{ user.address }}
+                  </label>
                   <input
+                    v-else
                     type="text"
                     class="form-control"
                     id="address"
@@ -90,7 +115,14 @@
                   >이메일
                 </label>
                 <div class="col-md-8">
+                  <label
+                    v-if="!modify"
+                    for="inputName"
+                    class="col-md-3 control-label text-md-right col-form-label"
+                    >{{ user.email }}
+                  </label>
                   <input
+                    v-else
                     type="text"
                     class="form-control"
                     id="email"
@@ -101,12 +133,20 @@
               </div>
             </form>
             <div class="row form-group">
-              <div class="ml-md-auto col-md-9">
-                <button class="btn btn-primary pr-3" @click="update()">
+              <div class="ml-md-auto col-md-9" v-if="!modify">
+                <button class="btn btn-primary pr-3" @click="reverseModify()">
                   수정 <i class="fa fa-check pl-3"></i>
                 </button>
                 <button class="btn btn-danger pr-3" @click="del()">
                   탈퇴 <i class="fa fa-check pl-3"></i>
+                </button>
+              </div>
+              <div class="ml-md-auto col-md-9" v-else>
+                <button class="btn btn-primary pr-3" @click="update()">
+                  완료 <i class="fa fa-check pl-3"></i>
+                </button>
+                <button class="btn btn-danger pr-3" @click="reverseModify()">
+                  취소 <i class="fa fa-check pl-3"></i>
                 </button>
               </div>
             </div>
@@ -124,7 +164,11 @@ export default {
   name: 'userinfo',
   data() {
     return {
+      modify: false,
       pwdcheck: '',
+      statusPwdMsg: '',
+      msgPwd: '',
+      okPwd: false,
     };
   },
   computed: {
@@ -135,30 +179,64 @@ export default {
       alert('로그인 해주세요.');
       this.$router.push('/login');
     }
-    console.log(this.user);
   },
   methods: {
-    update() {
-      //   if ($('#userName').val() == '') {
-      //     alert('이름 입력!!!');
-      //     return;
-      //   } else if ($('#userPwd').val() == '') {
-      //     alert('비밀번호 입력!!!');
-      //     return;
-      //   } else if ($('#userPwd').val() != $('#pwdcheck').val()) {
-      //     alert('비밀번호 확인!!!');
-      //     return;
-      //   } else if ($('address').val() == '') {
-      //     alert('주소 입력!!!');
-      //     return;
-      //   } else if ($('email').val() == '') {
-      //     alert('이메일 입력!!!');
-      //     return;
-      //   } else {
-      //     $('#loginform').action = '${root}/user/modify';
-      //     $('#loginform').submit();
-      //   }
+    reverseModify() {
+      this.user.userPwd = '';
+      this.pwdcheck = '';
+      this.modify = !this.modify;
     },
+    update() {
+      let err = true;
+      let msg = '';
+
+      if (this.user.userName == '') {
+        msg = '이름을 입력해주세요';
+        err = false;
+        this.$refs.userName.focus();
+      }
+
+      if (err && this.user.userPwd && !this.okPwd) {
+        msg = '비밀번호를 확인해주세요';
+        err = false;
+        this.$refs.pwdcheck.focus();
+      }
+
+      if (!err) {
+        alert(msg);
+      } else {
+        if (confirm('pwd : ' + this.user.userPwd)) {
+          axios
+            .put('http://localhost:8090/user/', {
+              userName: this.user.userName,
+              userId: this.user.userId,
+              userPwd: this.user.userPwd,
+              email: this.user.email,
+              address: this.user.address,
+            })
+            .then(() => {
+              alert('수정 성공');
+              // this.$router.push('/userInfo');
+              this.modify = !this.modify;
+              this.user.userPwd = '';
+              this.pwdcheck = '';
+            })
+            .catch(() => alert('수정 중 오류가 발생했습니다. 다시 시도해 주세요.'));
+        }
+      }
+    },
+    checkPwd() {
+      if (this.user.userPwd && this.user.userPwd == this.pwdcheck) {
+        this.msgPwd = '비밀번호가 일치합니다.';
+        this.statusPwdMsg = 'okMsg';
+        this.okPwd = true;
+      } else {
+        this.msgPwd = '비밀번호가 일치하지 않습니다.';
+        this.statusPwdMsg = 'waringMsg';
+        this.okPwd = false;
+      }
+    },
+
     del() {
       if (confirm('정말 탈퇴하시겠습니까?')) {
         axios
@@ -181,4 +259,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.waringMsg {
+  color: red;
+  font-size: 17px;
+}
+
+.okMsg {
+  color: rgb(105, 209, 0);
+  font-size: 17px;
+}
+</style>
