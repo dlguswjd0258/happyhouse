a21280813cfb2ead9084053dd94c0ac0537c4b04
adlgu <adlguswjda@naver.com>
2021-05-25 16:40:41 +0900

    add user search

--- happyhouse/src/components/user/adminuser.vue
+++ happyhouse/src/components/user/adminuser.vue
@@ -2,10 +2,10 @@
   <div class="container" align="center">
     <input type="hidden" name="pageNo" id="pageNo" />
     <div class="mb-3" align="right">
-      <b-form-select v-model="key" class="col-sd-2">
-        <b-form-select-option :value="all">--선택하세요--</b-form-select-option>
-        <b-form-select-option :value="userId">아이디</b-form-select-option>
-        <b-form-select-option :value="userName">이름</b-form-select-option>
+      <b-form-select v-model="bean.key" class="col-sd-2">
+        <b-form-select-option value="all">--선택하세요--</b-form-select-option>
+        <b-form-select-option value="userid">아이디</b-form-select-option>
+        <b-form-select-option value="username">이름</b-form-select-option>
       </b-form-select>
       <input type="text" class="col-sd-2" style="display: none" />
       <input
@@ -16,7 +16,7 @@
         name="word"
         v-model="bean.word"
       />
-      <a href="#" class="btn btn-primary col-sd-2" id="btn">검색</a>
+      <button type="button" class="btn btn-primary" @click="search()">검색</button>
     </div>
     <b-table-simple hover small borderless>
       <colgroup>
@@ -70,7 +70,6 @@
 
 <script>
 import axios from 'axios';
-// import axios from 'axios';
 export default {
   data() {
     return {
@@ -79,156 +78,32 @@ export default {
     };
   },
   methods: {
-    makeList() {
-      // $('#userlist').empty();
-      // $('#paging').empty();
-      // if (map.users != null) {
-      //   $(map.users).each(function (index, user) {
-      //     let str = `
-      // 	<tr id="view_${'${user.userId}'}" class="view" data-id="${'${user.userId}'}">
-      // 		<td>${'${user.userId}'}</td>
-      // 		<td>${'${user.userPwd}'}</td>
-      // 		<td>${'${user.userName}'}</td>
-      // 		<td>${'${user.email}'}</td>
-      // 		<td>${'${user.address}'}</td>
-      // 		<td>${'${user.joindate}'}</td>
-      // 		<td>
-      // 			<button type="button" class="modiBtn btn btn-outline-primary btn-sm">수정</button>
-      // 			<button type="button" class="delBtn btn btn-outline-danger btn-sm">삭제</button>
-      // 		</td>
-      // 	</tr>
-      // 	<tr id="mview_${'${user.userId}'}" data-id="${'${user.userId}'}" style="display: none;">
-      // 		<td>${'${user.userId}'}</td>
-      // 		<td><input type="text" name="userPwd" id="userPwd${'${user.userId}'}" value="${'${user.userPwd}'}"></td>
-      // 		<td>${'${user.userName}'}</td>
-      // 		<td><input type="text" name="email" id="email${'${user.userId}'}" value="${'${user.email}'}"></td>
-      // 		<td><input type="text" name="address" id="address${'${user.userId}'}" value="${'${user.address}'}"></td>
-      // 		<td>${'${user.joindate}'}</td>
-      // 		<td>
-      // 			<button type="button" class="modifyBtn btn btn-primary btn-sm">수정</button>
-      // 			<button type="button" class="cancelBtn btn btn-danger btn-sm">취소</button>
-      // 		</td>
-      // 	</tr>
-      // 	`;
-      //     $('#userlist').append(str);
-      //   }); //each
-      // } else {
-      //   let str = `
-      // <tr align="center">
-      // 	<td colspan="7">회원 정보가 없습니다.</td>
-      // </tr>
-      // `;
-      //   $('#userlist').append(str);
-      // }
+    search() {
+      if (!this.bean.word) {
+        alert('검색할 내용을 입력하세요.');
+      } else {
+        axios
+          .get(`http://localhost:8090/user/1/${this.bean.key}/${this.bean.word}`)
+          .then(({ data }) => {
+            this.users = data.users;
+            this.bean = data.bean;
+            this.bean.word = '';
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
   },
-  beforeCreate() {},
   created() {
-    this.bean.word = null;
-    axios.get(`http://localhost:8090/user/1/all/${this.bean.word}`).then(({ data }) => {
-      this.users = data.users;
-      this.bean = data.bean;
-    });
+    axios
+      .get(`http://localhost:8090/user/1/all/${null}`)
+      .then(({ data }) => {
+        this.users = data.users;
+        this.bean = data.bean;
+        this.bean.word = '';
+      })
+      .catch(() => alert('회원 조회 중 오류가 발생했습니다.'));
   },
 };
-// $(document).ready(function () {
-//   //회원 목록
-//   $.ajax({
-//     url: '${root}/user/1/all/""',
-//     type: 'GET',
-//     success: function (map) {
-//       makeList(map);
-//     },
-//     error: function (xhr, status, msg) {
-//       console.log('상태값 : ' + status + ' Http에러메시지 : ' + msg);
-//     },
-//     statusCode: {
-//       500: function () {
-//         alert('서버에러.');
-//         // location.href = "/error/e500";
-//       },
-//       404: function () {
-//         alert('페이지없다.');
-//         // location.href = "/error/e404";
-//       },
-//     },
-//   });
-
-//   $(document).on('dblclick', 'tr.view', function () {
-//     let vid = $(this).attr('data-id');
-//     $.ajax({
-//       url: '${root}/user/' + vid,
-//       type: 'GET',
-//       contentType: 'application/json;charset=utf-8',
-//       success: function (user) {
-//         $('#vid').text(user.userId);
-//         $('#vname').text(user.userName);
-//         $('#vemail').text(user.email);
-//         $('#vaddress').text(user.address);
-//         $('#vjoindate').text(user.joindate);
-//         // $("#userViewModal").modal();
-//       },
-//       error: function (xhr, status, msg) {
-//         console.log('상태값 : ' + status + ' Http에러메시지 : ' + msg);
-//       },
-//     });
-//   });
-
-//   /* // 회워 정보 수정 보기.
-// 		$(document).on("click", ".modiBtn", function() {
-// 			let mid = $(this).parents("tr").attr("data-id");
-// 			$("#view_" + mid).css("display", "none");
-// 			$("#mview_" + mid).css("display", "");
-// 		});
-
-// 		// 회워 정보 수정 실행.
-// 		$(document).on("click", ".modifyBtn", function() {
-// 			let mid = $(this).parents("tr").attr("data-id");
-// 			let modifyinfo = JSON.stringify({
-// 						"userId" : mid,
-// 						"userPwd" : $("#userPwd" + mid).val(),
-// 						"email" : $("#email" + mid).val(),
-// 						"address" : $("#address" + mid).val()
-// 					   });
-// 			$.ajax({
-// 				url:'${root}/admin/user',
-// 				type:'PUT',
-// 				contentType:'application/json;charset=utf-8',
-// 				dataType:'json',
-// 				data: modifyinfo,
-// 				success:function(users) {
-// 					makeList(users);
-// 				},
-// 				error:function(xhr,status,msg){
-// 					console.log("상태값 : " + status + " Http에러메시지 : "+msg);
-// 				}
-// 			});
-// 		});
-
-// 		// 회워 정보 수정 취소.
-// 		$(document).on("click", ".cancelBtn", function() {
-// 			let mid = $(this).parents("tr").attr("data-id");
-// 			$("#view_" + mid).css("display", "");
-// 			$("#mview_" + mid).css("display", "none");
-// 		});
-
-// 		// 회워 탈퇴.
-// 		$(document).on("click", ".delBtn", function() {
-// 			if(confirm("정말 삭제?")) {
-// 				let delid = $(this).parents("tr").attr("data-id");
-// 				$.ajax({
-// 					url:'${root}/admin/user/' + delid,
-// 					type:'DELETE',
-// 					contentType:'application/json;charset=utf-8',
-// 					dataType:'json',
-// 					success:function(users) {
-// 						makeList(users);
-// 					},
-// 					error:function(xhr,status,msg){
-// 						console.log("상태값 : " + status + " Http에러메시지 : "+msg);
-// 					}
-// 				});
-// 			}
-// 		}); */
-// });
 </script>
