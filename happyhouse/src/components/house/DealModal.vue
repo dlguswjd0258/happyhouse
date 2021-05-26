d82ca8ad76de854e98e52632c277976ceeb44530
adlgu <adlguswjda@naver.com>
2021-05-26 14:05:15 +0900

    modify dealmodal.vue

--- happyhouse/src/components/house/DealModal.vue
+++ happyhouse/src/components/house/DealModal.vue
@@ -3,49 +3,48 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-
-          <div class="modal-header">
-              {{deal.aptName}}
-          </div>
+          <h1 slot="header" class="modal-title">{{ deal.aptName }}</h1>
 
           <div class="modal-body">
-              <p>이미지</p>
-              <b-table-simple small>
-						<colgroup>
-							<col width="50%" />
-							<col width="50%" />
-						</colgroup>
-						<b-tbody>
-							<b-tr >
-								<b-td>거래금액</b-td>
-								<b-td>{{deal.dealAmount}},000</b-td>
-							</b-tr>
-							<b-tr >
-								<b-td>거래연도</b-td>
-								<b-td>{{deal.dealYear}}.{{deal.dealMonth}}.{{deal.dealDay}}</b-td>
-							</b-tr>
-							<b-tr >
-								<b-td>건축연도</b-td>
-								<b-td>{{deal.buildYear}}</b-td>
-							</b-tr>
-							<b-tr >
-								<b-td>층수</b-td>
-								<b-td>{{deal.floor}}</b-td>
-							</b-tr>
-							<b-tr >
-								<b-td>크기</b-td>
-								<b-td>{{deal.area}}</b-td>
-							</b-tr>
-						</b-tbody>
-					</b-table-simple>
+            <p>이미지</p>
+            <b-table-simple small>
+              <colgroup>
+                <col width="50%" />
+                <col width="50%" />
+              </colgroup>
+              <b-tbody>
+                <b-tr>
+                  <b-td>거래금액</b-td>
+                  <b-td>{{ deal.dealAmount }},000</b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td>거래연도</b-td>
+                  <b-td>{{ deal.dealYear }}.{{ deal.dealMonth }}.{{ deal.dealDay }}</b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td>건축연도</b-td>
+                  <b-td>{{ deal.buildYear }}</b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td>층수</b-td>
+                  <b-td>{{ deal.floor }}</b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td>크기</b-td>
+                  <b-td>{{ deal.area }}</b-td>
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
           </div>
 
           <div class="modal-footer">
-              <b-button variant="secondary" v-if="user.userId&&wish==false" @click="addWish">찜하기</b-button>
-              <b-button variant="danger" v-if="user&&wish==true" @click="deleteWish">찜취소</b-button>
-              <b-button variant="dark" @click="$emit('close')">
-                닫기
-              </b-button>
+            <b-button variant="secondary" v-if="user.userId && wish == false" @click="addWish"
+              >찜하기</b-button
+            >
+            <b-button variant="danger" v-if="user && wish == true" @click="deleteWish"
+              >찜취소</b-button
+            >
+            <b-button variant="dark" @click="$emit('close')"> 닫기 </b-button>
           </div>
         </div>
       </div>
@@ -55,64 +54,63 @@
 
 <script>
 import axios from 'axios';
-import {bus} from '@/dealbus'
+import { bus } from '@/dealbus';
 import { mapGetters } from 'vuex';
 
 export default {
   data() {
     return {
-		deal:{},
-    wish:false,
-		};
-	},
+      deal: {},
+      wish: false,
+    };
+  },
   created() {
-    console.log("생성");
-    bus.$on('showDeal',this.getDeal);
-    },
+    console.log('생성');
+    bus.$on('showDeal', this.getDeal);
+  },
   methods: {
-    getDeal(deal){
-      this.deal=deal;
+    getDeal(deal) {
+      this.deal = deal;
       console.log(this.user.userId);
-      if(this.user){
+      if (this.user) {
         axios
-        .get(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
-        .then(({data})=>{
-          if(data!=0){
-            this.wish=true;
-          }
-        })
-        .catch(()=>{
-          alert("찜 조회 중 오류 발생!")
-        })
+          .get(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
+          .then(({ data }) => {
+            if (data != 0) {
+              this.wish = true;
+            }
+          })
+          .catch(() => {
+            alert('찜 조회 중 오류 발생!');
+          });
       }
     },
-    addWish(){
+    addWish() {
       axios
-		  .post(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
-		  .then(()=>{
-        this.wish=true;
-		  })
-		  .catch(()=>{
-			  alert("찜 등록 중 오류 발생!")
-		  })
+        .post(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
+        .then(() => {
+          this.wish = true;
+        })
+        .catch(() => {
+          alert('찜 등록 중 오류 발생!');
+        });
     },
-    deleteWish(){
+    deleteWish() {
       axios
-		  .delete(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
-		  .then(()=>{
-        this.wish=false;
-		  })
-		  .catch(()=>{
-			  alert("찜 삭제 중 오류 발생!")
-		  })
+        .delete(`http://localhost:8090/house/wish/${this.user.userId}/${this.deal.no}`)
+        .then(() => {
+          this.wish = false;
+        })
+        .catch(() => {
+          alert('찜 삭제 중 오류 발생!');
+        });
     },
   },
   computed: {
     ...mapGetters(['user']),
   },
-}
+};
 </script>
-
 
 <style>
 .modal-mask {
@@ -122,9 +120,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: table;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
@@ -138,8 +136,8 @@ export default {
   padding: 20px 30px;
   background-color: #fff;
   border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
   font-family: Helvetica, Arial, sans-serif;
 }
 
