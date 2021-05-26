b81bfb791db0351d117541c9086c5ffb3d90d529
yhpgKIM <greenboy94@naver.com>
2021-05-26 16:25:57 +0900

    index

---
+++ happyhouse/src/components/indexm.vue
@@ -0,0 +1,43 @@
+!<template>
+	<div class="wrap">
+		<div class="image">
+			<b-img src="https://images.adsttc.com/media/images/5ecd/d4ac/b357/65c6/7300/009d/slideshow/02C.jpg?1590547607" fluid alt="Responsive image"></b-img>
+		</div>
+		<div class="text">
+			<h1>Happy House</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto&display=swap');.wrap {
+  width: 50%;
+  margin: 10px auto;
+  border: 1px solid #000000;
+  position: relative;
+}
+.wrap img {
+  width: 100%;
+  vertical-align: middle;
+}
+.text {
+  padding: 10px 20px;
+  border-radius: 10px;
+  text-align: center;
+  font-size: 100px;
+  font-family: 'Oswald', sans-serif;
+  font-family: 'Roboto', sans-serif;
+  color: #FFF;
+  position: absolute;
+  top: 23%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
