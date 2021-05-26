7e60b524de6104bfd5ed9cd74c64d20f60d88f9f
adlgu <adlguswjda@naver.com>
2021-05-26 14:04:25 +0900

    add template

--- happyhouse/src/plugins/dashboard-plugin.js
+++ happyhouse/src/plugins/dashboard-plugin.js
@@ -1,7 +1,7 @@
 // Polyfills for js features used in the Dashboard but not supported in some browsers (mainly IE)
-import '@/polyfills';
+// import '@/polyfills';
 // Notifications plugin. Used on Notifications page
-import Notifications from '@/components/NotificationPlugin';
+import Notifications from '@/components/templ/NotificationPlugin';
 // Validation plugin used to validate forms
 import { configure } from 'vee-validate';
 // A plugin file where you could register global components used across the app
@@ -9,7 +9,7 @@ import GlobalComponents from './globalComponents';
 // A plugin file where you could register global directives
 import GlobalDirectives from './globalDirectives';
 // Sidebar on the right. Used as a local plugin in DashboardLayout.vue
-import SideBar from '@/components/SidebarPlugin';
+// import SideBar from '@/components/SidebarPlugin';
 
 // element ui language configuration
 import lang from 'element-ui/lib/locale/lang/en';
@@ -17,7 +17,7 @@ import locale from 'element-ui/lib/locale';
 locale.use(lang);
 
 // vue-bootstrap
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 // asset imports
 import '@/assets/scss/argon.scss';
@@ -26,17 +26,17 @@ import { extend } from 'vee-validate';
 import * as rules from 'vee-validate/dist/rules';
 import { messages } from 'vee-validate/dist/locale/en.json';
 
-Object.keys(rules).forEach(rule => {
+Object.keys(rules).forEach((rule) => {
   extend(rule, {
     ...rules[rule], // copies rule configuration
-    message: messages[rule] // assign message
+    message: messages[rule], // assign message
   });
 });
 export default {
   install(Vue) {
     Vue.use(GlobalComponents);
     Vue.use(GlobalDirectives);
-    Vue.use(SideBar);
+    // Vue.use(SideBar);
     Vue.use(Notifications);
     Vue.use(BootstrapVue);
     Vue.use(IconsPlugin);
@@ -45,7 +45,7 @@ export default {
         valid: 'is-valid',
         invalid: 'is-invalid',
         dirty: ['is-dirty', 'is-dirty'], // multiple classes per flag!
-      }
-    })
-  }
+      },
+    });
+  },
 };
--- happyhouse/src/plugins/globalComponents.js
+++ happyhouse/src/plugins/globalComponents.js
@@ -1,17 +1,17 @@
-import BaseInput from '@/components/Inputs/BaseInput.vue';
-import BaseDropdown from '@/components/BaseDropdown.vue';
-import Card from '@/components/Cards/Card.vue';
-import Modal from '@/components/Modal.vue';
-import StatsCard from '@/components/Cards/StatsCard.vue';
-import BaseButton from '@/components/BaseButton.vue';
-import Badge from '@/components/Badge.vue';
-import BaseCheckbox from '@/components/Inputs/BaseCheckbox.vue';
-import BaseRadio from "@/components/Inputs/BaseRadio";
-import BaseProgress from "@/components/BaseProgress";
-import BasePagination from "@/components/BasePagination";
-import BaseAlert from "@/components/BaseAlert";
-import BaseNav from "@/components/Navbar/BaseNav";
-import BaseHeader from '@/components/BaseHeader';
+import BaseInput from '@/components/templ/Inputs/BaseInput.vue';
+import BaseDropdown from '@/components/templ/BaseDropdown.vue';
+import Card from '@/components/templ/Cards/Card.vue';
+import Modal from '@/components/templ/Modal.vue';
+import StatsCard from '@/components/templ/Cards/StatsCard.vue';
+import BaseButton from '@/components/templ/BaseButton.vue';
+import Badge from '@/components/templ/Badge.vue';
+// import BaseCheckbox from '@/components/Inputs/BaseCheckbox.vue';
+// import BaseRadio from '@/components/Inputs/BaseRadio';
+// import BaseProgress from '@/components/BaseProgress';
+import BasePagination from '@/components/templ/BasePagination';
+import BaseAlert from '@/components/templ/BaseAlert';
+import BaseNav from '@/components/templ/Navbar/BaseNav';
+import BaseHeader from '@/components/templ/BaseHeader';
 import { ValidationProvider, ValidationObserver } from 'vee-validate';
 import { Input, Tooltip, Popover } from 'element-ui';
 /**
@@ -23,23 +23,23 @@ const GlobalComponents = {
     Vue.component(Badge.name, Badge);
     Vue.component(BaseAlert.name, BaseAlert);
     Vue.component(BaseButton.name, BaseButton);
-    Vue.component(BaseCheckbox.name, BaseCheckbox);
+    // Vue.component(BaseCheckbox.name, BaseCheckbox);
     Vue.component(BaseHeader.name, BaseHeader);
     Vue.component(BaseInput.name, BaseInput);
     Vue.component(BaseDropdown.name, BaseDropdown);
     Vue.component(BaseNav.name, BaseNav);
     Vue.component(BasePagination.name, BasePagination);
-    Vue.component(BaseProgress.name, BaseProgress);
-    Vue.component(BaseRadio.name, BaseRadio);
+    // Vue.component(BaseProgress.name, BaseProgress);
+    // Vue.component(BaseRadio.name, BaseRadio);
     Vue.component(Card.name, Card);
     Vue.component(Modal.name, Modal);
     Vue.component(StatsCard.name, StatsCard);
     Vue.component(Input.name, Input);
-    Vue.component('validation-provider', ValidationProvider)
-    Vue.component('validation-observer', ValidationObserver)
+    Vue.component('validation-provider', ValidationProvider);
+    Vue.component('validation-observer', ValidationObserver);
     Vue.use(Tooltip);
     Vue.use(Popover);
-  }
+  },
 };
 
 export default GlobalComponents;
